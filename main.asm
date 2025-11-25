76116d8f82fcc57f16ce32f6312e97f5f5d11a01
PinJimWu <pinjim2006@gmail.com>
2025-11-26 01:10:03 +0800

    Feat: different tower and tower limit
    now you can change the tower limit by changing towerMax variable
    use a to e can place different types of towers
    change enter key detect into ELSEIF

--- main.asm
+++ main.asm
@@ -39,9 +39,10 @@ blockAttributes WORD blockWidth DUP(0F0h)     ; 白底黑字
 hConsole HANDLE ?
 
 ; Tower 相關
-towersPosX WORD 10 DUP(?)  ; 最多10個tower的X位置
-towersPosY WORD 10 DUP(?)  ; Y位置
-towersType BYTE 10 DUP(?)  ; 類型
+towerMax EQU 30
+towersPosX WORD towerMax DUP(?)  ; 最多10個tower的X位置
+towersPosY WORD towerMax DUP(?)  ; Y位置
+towersType BYTE towerMax DUP(?)  ; 類型
 towerCount DWORD 0
 tempBuffer BYTE blockWidth DUP(?)
 
@@ -138,41 +139,44 @@ START_MOVE:
     call ReadChar
 
     ; 檢查放置Tower的鍵
-    .IF ax == 1e61h
-        call addTower
+    .IF ax == 1e61h      ; a鍵
+        mov bl, 1
+        call addTowerWithType
+    .ELSEIF ax == 3062h  ; b鍵
+        mov bl, 2
+        call addTowerWithType
+    .ELSEIF ax == 2e63h  ; c鍵
+        mov bl, 3
+        call addTowerWithType
+    .ELSEIF ax == 2064h  ; d鍵
+        mov bl, 4
+        call addTowerWithType
+    .ELSEIF ax == 1265h  ; e鍵
+        mov bl, 5
+        call addTowerWithType
     .ENDIF
     
         ; 方向鍵檢測
-        ; UP
-    .IF ax == 4800h
+    .IF ax == 4800h ; UP
         sub blockPos.Y, blockHeight
         mov bx, blockPosInit.Y
         cmp blockPos.Y, bx
         jge END_MOVE
         add blockPos.Y, blockHeight
-    .ENDIF
-
-    ; DOWN
-    .IF ax == 5000h
+    .ELSEIF ax == 5000h ; DOWN
         add blockPos.Y, blockHeight
         mov bx, (blockHeight * 7)
         add bx, blockPosInit.Y
         cmp blockPos.Y, bx
         jbe END_MOVE
         sub blockPos.Y, blockHeight
-    .ENDIF
-
-    ; LEFT
-    .IF ax == 4B00h
+    .ELSEIF ax == 4B00h ; LEFT
         sub blockPos.X, blockWidth
         mov bx, blockPosInit.X
         cmp blockPos.X, bx
         jae END_MOVE
         add blockPos.X, blockWidth
-    .ENDIF
-
-    ; RIGHT
-    .IF ax == 4D00h
+    .ELSEIF ax == 4D00h ; RIGHT
         add blockPos.X, blockWidth
         mov bx, (blockWidth * 15)
         add bx, blockPosInit.X
@@ -193,15 +197,25 @@ EXIT_MOVE:
 moveBlock ENDP
 
 ;------------------------------------------------
-; 添加Tower (修正版本)
+; 添加Tower (舊版本，保留相容性)
 ;------------------------------------------------
 addTower PROC USES eax ebx esi
+    mov bl, 1  ; 預設類型
+    call addTowerWithType
+    ret
+addTower ENDP
+
+;------------------------------------------------
+; 添加指定類型的Tower
+;------------------------------------------------
+addTowerWithType PROC USES eax ecx esi
+    ; bl 包含塔的類型 (1-5)
     mov eax, towerCount
-    cmp eax, 10
-    jge NO_ADD
+    cmp eax, towerMax
+    jge NO_ADD_TYPE
     
     ; 保存當前索引
-    mov ebx, eax
+    mov ecx, eax
     
     ; 設定 X 座標
     mov esi, OFFSET towersPosX
@@ -212,22 +226,22 @@ addTower PROC USES eax ebx esi
     
     ; 設定 Y 座標
     mov esi, OFFSET towersPosY
-    mov eax, ebx
+    mov eax, ecx
     imul eax, 2  ; WORD 大小
     add esi, eax
     mov ax, blockPos.Y
     mov WORD PTR [esi], ax
     
-    ; 設定類型 (預設為類型 1)
+    ; 設定類型
     mov esi, OFFSET towersType
-    mov eax, ebx
+    mov eax, ecx
     add esi, eax  ; BYTE 大小
-    mov BYTE PTR [esi], 1
+    mov BYTE PTR [esi], bl
     
     inc towerCount
-NO_ADD:
+NO_ADD_TYPE:
     ret
-addTower ENDP
+addTowerWithType ENDP
 
 ;------------------------------------------------
 ; 畫單個Tower
@@ -260,36 +274,199 @@ drawTower PROC USES eax ebx ecx edi esi
     add esi, eax  ; BYTE 大小
     mov bl, BYTE PTR [esi]
 
-    ; 畫頂：=====
-    INVOKE WriteConsoleOutputAttribute, outputHandle, ADDR blockAttributes, blockWidth, outerBoxPos, ADDR cellsWritten
-    mov al, '='
-    mov ecx, blockWidth
-    lea edi, tempBuffer
-    rep stosb
-    INVOKE WriteConsoleOutputCharacter, outputHandle, ADDR tempBuffer, blockWidth, outerBoxPos, ADDR count
-    inc outerBoxPos.Y
-
-    ; 中： } {
-    INVOKE WriteConsoleOutputAttribute, outputHandle, ADDR blockAttributes, blockWidth, outerBoxPos, ADDR cellsWritten
-    mov tempBuffer, ' '
-    mov tempBuffer+1, '}'  
-    mov tempBuffer+2, ' '
-    mov tempBuffer+3, '{'
-    mov tempBuffer+4, ' '
-    INVOKE WriteConsoleOutputCharacter, outputHandle, ADDR tempBuffer, blockWidth, outerBoxPos, ADDR count
-    inc outerBoxPos.Y
-
-    ; 底：=====
-    INVOKE WriteConsoleOutputAttribute, outputHandle, ADDR blockAttributes, blockWidth, outerBoxPos, ADDR cellsWritten
-    mov al, '='
-    mov ecx, blockWidth
-    lea edi, tempBuffer
-    rep stosb
-    INVOKE WriteConsoleOutputCharacter, outputHandle, ADDR tempBuffer, blockWidth, outerBoxPos, ADDR count
+    ; 根據類型畫不同樣式的塔
+    .IF bl == 1      ; a鍵
+        call drawBasicTower
+    .ELSEIF bl == 2  ; b鍵
+        call drawDefenseTower
+    .ELSEIF bl == 3  ; c鍵
+        call drawAttackTower
+    .ELSEIF bl == 4  ; d鍵
+        call drawMagicTower
+    .ELSEIF bl == 5  ; e鍵
+        call drawSpecialTower
+    .ELSE            ; 預設
+        call drawBasicTower
+    .ENDIF
     
     pop DWORD PTR outerBoxPos
     ret 4  ; 清除參數
 drawTower ENDP
+
+;------------------------------------------------
+; 繪製a鍵
+;------------------------------------------------
+drawBasicTower PROC
+    ; 頂：█████
+    INVOKE WriteConsoleOutputAttribute, outputHandle, ADDR blockAttributes, blockWidth, outerBoxPos, ADDR cellsWritten
+    mov tempBuffer, 0DBh    ; █
+    mov tempBuffer+1, 0DBh  ; █
+    mov tempBuffer+2, 0DBh  ; █
+    mov tempBuffer+3, 0DBh  ; █
+    mov tempBuffer+4, 0DBh  ; █
+    INVOKE WriteConsoleOutputCharacter, outputHandle, ADDR tempBuffer, blockWidth, outerBoxPos, ADDR count
+    inc outerBoxPos.Y
+
+    ; 中：█ ● █
+    INVOKE WriteConsoleOutputAttribute, outputHandle, ADDR blockAttributes, blockWidth, outerBoxPos, ADDR cellsWritten
+    mov tempBuffer, 0DBh    ; █
+    mov tempBuffer+1, ' '
+    mov tempBuffer+2, 07h   ; ●
+    mov tempBuffer+3, ' '
+    mov tempBuffer+4, 0DBh  ; █
+    INVOKE WriteConsoleOutputCharacter, outputHandle, ADDR tempBuffer, blockWidth, outerBoxPos, ADDR count
+    inc outerBoxPos.Y
+
+    ; 底：█████
+    INVOKE WriteConsoleOutputAttribute, outputHandle, ADDR blockAttributes, blockWidth, outerBoxPos, ADDR cellsWritten
+    mov tempBuffer, 0DBh    ; █
+    mov tempBuffer+1, 0DBh  ; █
+    mov tempBuffer+2, 0DBh  ; █
+    mov tempBuffer+3, 0DBh  ; █
+    mov tempBuffer+4, 0DBh  ; █
+    INVOKE WriteConsoleOutputCharacter, outputHandle, ADDR tempBuffer, blockWidth, outerBoxPos, ADDR count
+    ret
+drawBasicTower ENDP
+
+;------------------------------------------------
+; 繪製b鍵
+;------------------------------------------------
+drawDefenseTower PROC
+    ; 頂：▲▲▲▲▲
+    INVOKE WriteConsoleOutputAttribute, outputHandle, ADDR blockAttributes, blockWidth, outerBoxPos, ADDR cellsWritten
+    mov tempBuffer, 1Eh     ; ▲
+    mov tempBuffer+1, 1Eh   ; ▲
+    mov tempBuffer+2, 1Eh   ; ▲
+    mov tempBuffer+3, 1Eh   ; ▲
+    mov tempBuffer+4, 1Eh   ; ▲
+    INVOKE WriteConsoleOutputCharacter, outputHandle, ADDR tempBuffer, blockWidth, outerBoxPos, ADDR count
+    inc outerBoxPos.Y
+
+    ; 中：║ ♦ ║
+    INVOKE WriteConsoleOutputAttribute, outputHandle, ADDR blockAttributes, blockWidth, outerBoxPos, ADDR cellsWritten
+    mov tempBuffer, 0BAh    ; ║
+    mov tempBuffer+1, ' '
+    mov tempBuffer+2, 04h   ; ♦
+    mov tempBuffer+3, ' '
+    mov tempBuffer+4, 0BAh  ; ║
+    INVOKE WriteConsoleOutputCharacter, outputHandle, ADDR tempBuffer, blockWidth, outerBoxPos, ADDR count
+    inc outerBoxPos.Y
+
+    ; 底：═════
+    INVOKE WriteConsoleOutputAttribute, outputHandle, ADDR blockAttributes, blockWidth, outerBoxPos, ADDR cellsWritten
+    mov tempBuffer, 0CDh    ; ═
+    mov tempBuffer+1, 0CDh  ; ═
+    mov tempBuffer+2, 0CDh  ; ═
+    mov tempBuffer+3, 0CDh  ; ═
+    mov tempBuffer+4, 0CDh  ; ═
+    INVOKE WriteConsoleOutputCharacter, outputHandle, ADDR tempBuffer, blockWidth, outerBoxPos, ADDR count
+    ret
+drawDefenseTower ENDP
+
+;------------------------------------------------
+; 繪製c鍵
+;------------------------------------------------
+drawAttackTower PROC
+    ; 頂： ╔═╗ 
+    INVOKE WriteConsoleOutputAttribute, outputHandle, ADDR blockAttributes, blockWidth, outerBoxPos, ADDR cellsWritten
+    mov tempBuffer, ' '
+    mov tempBuffer+1, 0C9h  ; ╔
+    mov tempBuffer+2, 0CDh  ; ═
+    mov tempBuffer+3, 0BBh  ; ╗
+    mov tempBuffer+4, ' '
+    INVOKE WriteConsoleOutputCharacter, outputHandle, ADDR tempBuffer, blockWidth, outerBoxPos, ADDR count
+    inc outerBoxPos.Y
+
+    ; 中：►♠ ◄
+    INVOKE WriteConsoleOutputAttribute, outputHandle, ADDR blockAttributes, blockWidth, outerBoxPos, ADDR cellsWritten
+    mov tempBuffer, 10h     ; ►
+    mov tempBuffer+1, 06h   ; ♠
+    mov tempBuffer+2, ' '
+    mov tempBuffer+3, 06h   ; ♠
+    mov tempBuffer+4, 11h   ; ◄
+    INVOKE WriteConsoleOutputCharacter, outputHandle, ADDR tempBuffer, blockWidth, outerBoxPos, ADDR count
+    inc outerBoxPos.Y
+
+    ; 底： ╚═╝ 
+    INVOKE WriteConsoleOutputAttribute, outputHandle, ADDR blockAttributes, blockWidth, outerBoxPos, ADDR cellsWritten
+    mov tempBuffer, ' '
+    mov tempBuffer+1, 0C8h  ; ╚
+    mov tempBuffer+2, 0CDh  ; ═
+    mov tempBuffer+3, 0BCh  ; ╝
+    mov tempBuffer+4, ' '
+    INVOKE WriteConsoleOutputCharacter, outputHandle, ADDR tempBuffer, blockWidth, outerBoxPos, ADDR count
+    ret
+drawAttackTower ENDP
+
+;------------------------------------------------
+; 繪製d鍵
+;------------------------------------------------
+drawMagicTower PROC
+    ; 頂：※※※※※
+    INVOKE WriteConsoleOutputAttribute, outputHandle, ADDR blockAttributes, blockWidth, outerBoxPos, ADDR cellsWritten
+    mov tempBuffer, 15h     ; ※
+    mov tempBuffer+1, 15h   ; ※
+    mov tempBuffer+2, 15h   ; ※
+    mov tempBuffer+3, 15h   ; ※
+    mov tempBuffer+4, 15h   ; ※
+    INVOKE WriteConsoleOutputCharacter, outputHandle, ADDR tempBuffer, blockWidth, outerBoxPos, ADDR count
+    inc outerBoxPos.Y
+
+    ; 中：~ ☼ ~
+    INVOKE WriteConsoleOutputAttribute, outputHandle, ADDR blockAttributes, blockWidth, outerBoxPos, ADDR cellsWritten
+    mov tempBuffer, 7Eh     ; ~
+    mov tempBuffer+1, ' '
+    mov tempBuffer+2, 0Fh   ; ☼
+    mov tempBuffer+3, ' '
+    mov tempBuffer+4, 7Eh   ; ~
+    INVOKE WriteConsoleOutputCharacter, outputHandle, ADDR tempBuffer, blockWidth, outerBoxPos, ADDR count
+    inc outerBoxPos.Y
+
+    ; 底：∩∩∩∩∩
+    INVOKE WriteConsoleOutputAttribute, outputHandle, ADDR blockAttributes, blockWidth, outerBoxPos, ADDR cellsWritten
+    mov tempBuffer, 0E9h    ; ∩
+    mov tempBuffer+1, 0E9h  ; ∩
+    mov tempBuffer+2, 0E9h  ; ∩
+    mov tempBuffer+3, 0E9h  ; ∩
+    mov tempBuffer+4, 0E9h  ; ∩
+    INVOKE WriteConsoleOutputCharacter, outputHandle, ADDR tempBuffer, blockWidth, outerBoxPos, ADDR count
+    ret
+drawMagicTower ENDP
+
+;------------------------------------------------
+; 繪製e鍵
+;------------------------------------------------
+drawSpecialTower PROC
+    ; 頂：♫♪♫♪♫
+    INVOKE WriteConsoleOutputAttribute, outputHandle, ADDR blockAttributes, blockWidth, outerBoxPos, ADDR cellsWritten
+    mov tempBuffer, 0Eh     ; ♫
+    mov tempBuffer+1, 0Dh   ; ♪
+    mov tempBuffer+2, 0Eh   ; ♫
+    mov tempBuffer+3, 0Dh   ; ♪
+    mov tempBuffer+4, 0Eh   ; ♫
+    INVOKE WriteConsoleOutputCharacter, outputHandle, ADDR tempBuffer, blockWidth, outerBoxPos, ADDR count
+    inc outerBoxPos.Y
+
+    ; 中：◄ ♥ ►
+    INVOKE WriteConsoleOutputAttribute, outputHandle, ADDR blockAttributes, blockWidth, outerBoxPos, ADDR cellsWritten
+    mov tempBuffer, 11h     ; ◄
+    mov tempBuffer+1, ' '
+    mov tempBuffer+2, 03h   ; ♥
+    mov tempBuffer+3, ' '
+    mov tempBuffer+4, 10h   ; ►
+    INVOKE WriteConsoleOutputCharacter, outputHandle, ADDR tempBuffer, blockWidth, outerBoxPos, ADDR count
+    inc outerBoxPos.Y
+
+    ; 底：▼▼▼▼▼
+    INVOKE WriteConsoleOutputAttribute, outputHandle, ADDR blockAttributes, blockWidth, outerBoxPos, ADDR cellsWritten
+    mov tempBuffer, 1Fh     ; ▼
+    mov tempBuffer+1, 1Fh   ; ▼
+    mov tempBuffer+2, 1Fh   ; ▼
+    mov tempBuffer+3, 1Fh   ; ▼
+    mov tempBuffer+4, 1Fh   ; ▼
+    INVOKE WriteConsoleOutputCharacter, outputHandle, ADDR tempBuffer, blockWidth, outerBoxPos, ADDR count
+    ret
+drawSpecialTower ENDP
 
 ;------------------------------------------------
 ; 畫所有Tower
